c9b2f548976b598d7a9505be282d9021b174fc7c
Steven Wong <ywong15@smpanthers.org>
2014-05-12 14:06:00 -0700

    code.js

---
+++ code.js
@@ -0,0 +1,16 @@
+function changetext() {
+	 alert("HAHAHAHAHAHAHAHAHAHAHAHA");
+	 $("#foo").html("hi im steven");
+	 //$("#foo").fadeOut();
+       	 //$("p").addClass("nice");
+                        }
+
+ function changetext2() {
+	 $("#foo1").html("Well Play");
+         $("#foo1").css("color","blue");
+         $("#fool").css("fontSize","50px");
+                        }
+
+function changetext3() {
+	 $("#foo2").html("http://ywong15.github.io/lyrics.html");
+                        }
