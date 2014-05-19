5e979013fb7be3723e4c7eda9501530112492359
Steven Wong <ywong15@smpanthers.org>
2014-05-19 13:54:55 -0700

    collatz.js

---
+++ collatZ.js
@@ -0,0 +1,15 @@
+var collatz = fuction(n){
+	var output = "collatz("+n+")[";
+	cosole.log(legth + n);
+	while(n > 1){
+	 if(n % 2 === 0){ n = n/2;
+}
+	 output += 1;
+	else{ n = 3*n + 1;
+
+	 output += 1;
+}
+}
+}
+	output += "]";
+	console.log(output);
